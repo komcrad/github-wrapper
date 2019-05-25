cb08371c2cd942181790c29be162e61d376e48ab
komcrad <konradt95@gmail.com>
2019-05-25 14:31:58 -0500

    Throws exception if access token is not set. Adds function docstrings.

--- src/github_wrapper/core.clj
+++ src/github_wrapper/core.clj
@@ -13,30 +13,45 @@
   (reset! token (first args)))
 
 (defn authed-get [url]
-  (http/get url {:headers {"Authorization"
-                           (str "token " @token)}}))
+  (if (empty? @token)
+    (throw (Exception. "token not defined"))
+    (http/get url {:headers {"Authorization"
+                             (str "token " @token)}})))
 
-(defn get-followers [user]
+(defn get-followers
+  "Returns a future containing followers of user"
+  [user]
   (authed-get (str "https://api.github.com/users/" user
                     "/followers?per_page=5")))
 
-(defn parse-followers [followers]
+(defn parse-followers
+  "Resolves a future containing futures and returns the information
+   as a map"
+  [followers]
   (map #(do {:id (:login %)})
        (keywordize-keys (json/read-str (:body @followers)))))
 
-(defn assoc-follows-async [m]
+(defn assoc-follows-async
+  "Given a map with {:id github-user-name} associates a :followers
+   key. The value of this key will be a future."
+  [m]
   (postwalk #(cond
                (and (map? %) (nil? (:followers %)))
                (assoc % :followers (get-followers (:id %)))
                :else %) m))
 
-(defn resolve-async-follows [m]
+(defn resolve-async-follows
+  "Given a map, resolves all futures into lists of user ids"
+  [m]
   (postwalk #(cond
                (s/includes? (str (type %)) "org.httpkit.client$deadlock")
                (parse-followers %)
                :else %) m))
 
-(defn followers [user]
+(defn followers
+  "given a github usernames, returns that users followers and their followers
+   recursivley 3 levels deep. Each level's followers are gathered in parallel"
+  [user]
   (loop [m {:id user} depth 0]
     (if (= 3 depth)
       m
